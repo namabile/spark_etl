cad071346bdbfef0454cbe96b7614cf1c39508ee
Nick Amabile <amabile.n@gmail.com>
2015-10-03 16:01:12 +0000

    wip to write parquet

--- build.sbt
+++ build.sbt
@@ -1,12 +1,15 @@
 // vi:syntax=scala
 
-lazy val twitter4j = "org.twitter4j" % "twitter4j-stream" % "4.0.4"
-lazy val configFactory = "com.typesafe" % "config" % "1.3.0"
-lazy val sparkCore = "org.apache.spark" %% "spark-core" % "1.5.0" % "provided"
-lazy val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "1.5.0" % "provided"
-lazy val sparkStreamingKafka = "org.apache.spark" %% "spark-streaming-kafka" % "1.5.0"
-lazy val avro = "org.apache.avro" % "avro" % "1.7.7"
-lazy val hadoopClient = "org.apache.hadoop" % "hadoop-hdfs" % "2.6.0-cdh5.4.7" % "provided"
+lazy val deps = Seq(
+  "org.twitter4j" % "twitter4j-stream" % "4.0.4",
+  "com.typesafe" % "config" % "1.3.0",
+  "org.apache.spark" %% "spark-core" % "1.5.0" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "1.5.0" % "provided",
+  "org.apache.spark" %% "spark-sql" % "1.5.0",
+  "org.apache.spark" %% "spark-streaming-kafka" % "1.5.0",
+  "org.apache.avro" % "avro" % "1.7.7",
+  "org.apache.hadoop" % "hadoop-hdfs" % "2.6.0-cdh5.4.7" % "provided"
+  )
 
 lazy val root = 
   (project in file(".")).
@@ -17,15 +20,7 @@ lazy val root =
       scalaVersion := "2.11.6",
       javaOptions += "-Xmx2g",
       resolvers += "Cloudera CDH 5" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
-      libraryDependencies ++= Seq(
-        twitter4j, 
-        configFactory,
-        sparkCore,
-        sparkStreaming,
-        sparkStreamingKafka,
-        avro,
-        hadoopClient
-      ),
+      libraryDependencies ++= deps,
       mergeStrategy in assembly := {
         case PathList("org", "apache", "spark", "unused", "UnusedStubClass.class") => MergeStrategy.first
         case x => (mergeStrategy in assembly).value(x)
--- src/main/scala/com/namabile/ConsumeTwitterStream.scala
+++ src/main/scala/com/namabile/ConsumeTwitterStream.scala
@@ -10,14 +10,16 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+import org.apache.spark.rdd.RDD
 
 object ConsumeTwitterStream extends App {
   private val conf = ConfigFactory.load()
   private val sparkMaster = conf.getString("addresses.spark_master")
 
   private val sparkConf = new SparkConf().setAppName("WindowTweetCount").setMaster(sparkMaster)
-
   private val ssc = new StreamingContext(sparkConf, Seconds(2))
+  private val sc = new SparkContext(sparkConf)
+  private val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
   ssc.checkpoint("hdfs://ip-10-0-0-127.ec2.internal:8020/user/root/checkPointDir")
 
@@ -34,10 +36,22 @@ object ConsumeTwitterStream extends App {
   val lines = KafkaUtils.createStream[String, Array[Byte], DefaultDecoder, DefaultDecoder](ssc, kafkaConf, topicMap, StorageLevel.MEMORY_ONLY_SER).map(_._2)
 
   // Get a count of the tweets per user in the last 10 minutes, refreshing every 2 seconds
-  val tweetRDD = lines.map{ bytes: Array[Byte] => tweetDecode(bytes) }.map{ tweet: Tweet => (tweet.id, 1L) }
-  val tweetCounts = tweetRDD.reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
+  val tweetRDD = lines.map{ bytes: Array[Byte] => tweetDecode(bytes) }
+  val tweetCounts = tweetRDD.map{ tweet: Tweet => (tweet.name.toString, 1L) }.reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
 
-  tweetCounts.print // Print out the results.  Or we can produce new kafka events containing the mapped ids.
+  // Print out the counts
+  tweetCounts.print
+
+  // write a parquet file to hdfs every 5 minutes
+  tweetRDD.window(Minutes(5)).foreachRDD {
+    rdd: RDD[Tweet] =>
+      val path = "hdfs://ip-10-0-0-127.ec2.internal:8020/user/root/tweets"
+      val timestamp: Long = System.currentTimeMillis / 1000
+      val prefix = "tweets-" + timestamp
+      val suffix = ".parquet"
+      rdd.write.parquet(prefix + suffix)
+      println("file written!")
+  }
 
   ssc.start()
   ssc.awaitTermination()
